bef8740874db75dc39d3458d6f369c082f4f8b69
vladbuk <vladbuk@gmail.com>
2023-02-13 13:46:03 +0200

    subdomain without elb

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,13 +3,13 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
-# resource "aws_route53_record" "test" {
-#   zone_id = data.aws_route53_zone.selected.zone_id
-#   name    = "test.${data.aws_route53_zone.selected.name}"
-#   type    = "A"
-#   ttl     = 600
-#   records = [aws_instance.t2micro_ubuntu_test.public_ip]
-# }
+resource "aws_route53_record" "testip" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "testip.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = 600
+  records = [aws_instance.t2micro_ubuntu_test.public_ip]
+}
 
 resource "aws_route53_record" "test" {
   # for_each = {
@@ -24,6 +24,14 @@ resource "aws_route53_record" "test" {
   type    = "CNAME"
   ttl     = 600
   records = [ aws_alb.alb.dns_name ]
+}
+
+resource "aws_route53_record" "prodip" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = "prodip.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = 600
+  records = [aws_instance.t2micro_ubuntu_prod.public_ip]
 }
 
 resource "aws_route53_record" "prod" {
