54a1798548044c038330d901ef3936520f19f015
vladbuk <vladbuk@gmail.com>
2023-02-14 21:05:59 +0200

    cleaning

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -52,10 +52,7 @@ resource "aws_alb_listener_rule" "listener_test_rule" {
   depends_on   = [ aws_alb_target_group.test ]
   listener_arn = aws_alb_listener.alb_listener.arn
   priority     = 10
-  # action {
-  #   type             = "forward"
-  #   target_group_arn = aws_alb_target_group.test.arn
-  # }  
+
   action {
     type = "redirect"
 
@@ -170,95 +167,3 @@ resource "aws_alb_target_group_attachment" "prod" {
   target_id        = aws_instance.t2micro_ubuntu_prod.id
   port             = 8080
 }
-
-
-
-# # http listener
-# resource "aws_alb_listener" "alb_prod_listener" {
-#   load_balancer_arn = aws_alb.alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-  
-#   default_action {
-#     target_group_arn = aws_alb_target_group.prod.arn
-#     type             = "forward"
-#   }
-# }
-
-# resource "aws_alb_listener_rule" "prod_listener_rule" {
-#   depends_on   = [ aws_alb_target_group.prod ]
-#   listener_arn = aws_alb_listener.alb_prod_listener.arn
-#   priority     = 10
-#   action {
-#     type = "redirect"
-
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   } 
-#   condition {
-#     host_header {
-#       values = ["vladbuk.site"]
-#     }
-#   }
-# }
-
-# # https listener
-# resource "aws_alb_listener" "alb_prod_https_listener" {
-#   load_balancer_arn = aws_alb.alb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   certificate_arn = "arn:aws:acm:eu-central-1:054889260026:certificate/c4d4b5f1-b623-43fb-aed4-761d6d294897"
-  
-#   default_action {
-#     target_group_arn = aws_alb_target_group.prod.arn
-#     type             = "forward"
-#   }
-# }
-
-# resource "aws_alb_listener_rule" "https_prod_listener_rule" {
-#   depends_on   = [ aws_alb_target_group.prod ]
-#   listener_arn = aws_alb_listener.alb_prod_https_listener.arn
-#   priority     = 10
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.prod.arn
-#   }   
-#   condition {
-#     host_header {
-#       values = ["vladbuk.site"]
-#     }
-#   }
-# }
-
-###################################
-
-
-# https listener
-
-# resource "aws_acm_certificate" "test_cert" {
-#   domain_name       = "test.vladbuk.site"
-#   validation_method = "DNS"
-
-#   tags = {
-#     name = "test-cert"
-#   }
-# }
-
-# resource "aws_alb_target_group" "https_test" {
-#   name     = "https-test"
-#   port     = "443"
-#   protocol = "HTTPS"
-#   vpc_id   = aws_vpc.main_vpc.id
-#   tags = {
-#     name = "https-test"
-#   }   
-# }
-
-# resource "aws_alb_target_group_attachment" "https_test" {
-#   target_group_arn = aws_alb_target_group.https_test.arn
-#   target_id        = aws_instance.t2micro_ubuntu_test.id
-#   port             = 8080
-# }
