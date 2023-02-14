0e73f1faf9d0b6af40ee509ab8934a1333d1b5e2
vladbuk <vladbuk@gmail.com>
2023-02-14 10:50:41 +0200

    prod target group

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -9,6 +9,24 @@ resource "aws_alb" "alb" {
   }
 }
 
+# testing target group
+resource "aws_alb_target_group" "test" {
+  name     = "test"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+  tags = {
+    name = "test"
+  }   
+}
+
+resource "aws_alb_target_group_attachment" "test" {
+  target_group_arn = aws_alb_target_group.test.arn
+  target_id        = aws_instance.t2micro_ubuntu_test.id
+  port             = 8080
+}
+
+# http listener
 resource "aws_alb_listener" "alb_listener" {
   load_balancer_arn = aws_alb.alb.arn
   port              = "80"
@@ -54,35 +72,7 @@ resource "aws_alb_listener_rule" "listener_rule" {
   }
 }
 
-resource "aws_alb_target_group" "test" {
-  name     = "test"
-  port     = "80"
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main_vpc.id
-  tags = {
-    name = "test"
-  }   
-}
-
-resource "aws_alb_target_group_attachment" "test" {
-  target_group_arn = aws_alb_target_group.test.arn
-  target_id        = aws_instance.t2micro_ubuntu_test.id
-  port             = 8080
-}
-
-
-
 # https listener
-
-# resource "aws_acm_certificate" "test_cert" {
-#   domain_name       = "test.vladbuk.site"
-#   validation_method = "DNS"
-
-#   tags = {
-#     name = "test-cert"
-#   }
-# }
-
 resource "aws_alb_listener" "alb_https_listener" {
   load_balancer_arn = aws_alb.alb.arn
   port              = "443"
@@ -109,6 +99,98 @@ resource "aws_alb_listener_rule" "https_listener_rule" {
     }
   }
 }
+
+
+# PRODUCTION target group
+resource "aws_alb_target_group" "prod" {
+  name     = "prod"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+  tags = {
+    name = "prod"
+  }   
+}
+
+resource "aws_alb_target_group_attachment" "prod" {
+  target_group_arn = aws_alb_target_group.prod.arn
+  target_id        = aws_instance.t2micro_ubuntu_prod.id
+  port             = 8080
+}
+
+# http listener
+resource "aws_alb_listener" "alb_prod_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    target_group_arn = aws_alb_target_group.prod.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener_rule" "listener_rule" {
+  depends_on   = [ aws_alb_target_group.prod ]
+  listener_arn = aws_alb_listener.alb_prod_listener.arn
+  priority     = 10
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  } 
+  condition {
+    host_header {
+      values = ["vladbuk.site"]
+    }
+  }
+}
+
+# https listener
+resource "aws_alb_listener" "alb_prod_https_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn = "arn:aws:acm:eu-central-1:054889260026:certificate/c4d4b5f1-b623-43fb-aed4-761d6d294897"
+  
+  default_action {
+    target_group_arn = aws_alb_target_group.prod.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener_rule" "https_prod_listener_rule" {
+  depends_on   = [ aws_alb_target_group.prod ]
+  listener_arn = aws_alb_listener.alb_prod_https_listener.arn
+  priority     = 10
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.prod.arn
+  }   
+  condition {
+    host_header {
+      values = ["vladbuk.site"]
+    }
+  }
+}
+
+
+
+
+# https listener
+
+# resource "aws_acm_certificate" "test_cert" {
+#   domain_name       = "test.vladbuk.site"
+#   validation_method = "DNS"
+
+#   tags = {
+#     name = "test-cert"
+#   }
+# }
 
 # resource "aws_alb_target_group" "https_test" {
 #   name     = "https-test"
